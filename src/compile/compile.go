65e1f6d941a5fbc2d4d1481b1c25d664550045b9
Matthew Horan <mhoran@pivotal.io>
2017-02-02 10:56:02 -0500

    WIP; download dependency from manifet.yml
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- src/compile/compile.go
+++ src/compile/compile.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"archive/zip"
+	. "common"
 	"errors"
+	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
-	. "common"
+	yaml "github.com/go-yaml/yaml"
+	"github.com/sesmith177/go-ce-test/buildpack"
 )
 
 func main() {
 	buildDir, _, err := parseArgs(os.Args[1:])
+	CheckErr(err)
+
+	var m buildpack.Manifest
+
+	buildpackDir, err := filepath.Abs(filepath.Join(filepath.Dir(os.Args[0]), ".."))
+	CheckErr(err)
+
+	manifestData, err := ioutil.ReadFile(filepath.Join(buildpackDir, "manifest.yml"))
+	CheckErr(err)
+
+	err = yaml.Unmarshal(manifestData, &m)
 	CheckErr(err)
 
 	CheckWebConfig(buildDir)
@@ -18,13 +34,41 @@ func main() {
 	err = os.MkdirAll(hwcDir, 0700)
 	CheckErr(err)
 
-	binDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	downloader := buildpack.NewDownloader(hwcDir, &m)
+	dest, err := downloader.Fetch(buildpack.Dependency{Name: "hwc", Version: "1.0.0"}, "hwc.zip")
 	CheckErr(err)
 
-	err = os.Rename(filepath.Join(binDir, "hwc.exe"), filepath.Join(hwcDir, "hwc.exe"))
+	err = unzipHWC(dest, filepath.Join(hwcDir, "hwc.exe"))
+
 	CheckErr(err)
 
 	os.Exit(0)
+}
+
+func unzipHWC(src string, output string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+
+		f2, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		defer f2.Close()
+
+		_, err = io.Copy(f2, rc)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func parseArgs(args []string) (string, string, error) {
